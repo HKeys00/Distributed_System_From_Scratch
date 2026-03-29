9ec8596842fb16da3b80778e3febca1347efdf98
Harry Keys <harry.keys40@gmail.com>
2026-03-30 07:05:51 +1100

    Moving from in memory ingress queue to persistent Postgres db with outbox pattern

---
+++ Data/Models/WorkItem.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Models
+{
+    /// <summary>
+    /// Represents a unit of work with associated metadata, payload, and execution details.
+    /// </summary>
+    public class WorkItem
+    {
+        /// <summary>
+        /// Gets or sets the ordering identifier for the task.
+        /// </summary>
+        [Column(TypeName = "int8")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the task.
+        /// </summary>
+        public Guid TaskId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of task this work item corresponds to.
+        /// </summary>
+        public required string TaskType { get; set; }
+
+        /// <summary>
+        /// The type of execution model this work item belongs to.
+        /// </summary>
+        public required string ExecutionType { get; set; }
+
+        /// <summary>
+        /// The payload data of the work item.
+        /// </summary>
+        [Column(TypeName = "jsonb")]
+        public required dynamic Payload { get; set; }
+
+        /// <summary>
+        /// When this work item was created.
+        /// </summary>
+        [Column(TypeName = "timestamptz")]
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// When this work item was published to the outbox.
+        /// </summary>
+        [Column(TypeName = "timestamptz")]
+        public DateTime PublishedAt { get; set; }
+
+        /// <summary>
+        /// The number of retries this work item has undergone.
+        /// </summary>
+        public int Retries { get; set; }
+    }
+}
